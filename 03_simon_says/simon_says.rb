2fb3b3a76c522fb053b1c801186d0f62ba99214b
Hershel Nashman <hershel.nashman@gmail.com>
2015-11-02 16:24:56 -0500

    echo, shout, start_of_word, first_word all pass

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,9 +6,23 @@ def shout(string)
 	string.upcase
 end
 
-def repeat(string)
-	"#{string} #{string}"
+def repeat(string, n=2)
+	[string] * n * ' '
 end
+
+def start_of_word(string, num)
+	array = string.split('')
+	array[0...num].join("")
+end
+
+def first_word(string)
+	array = string.split(' ')
+	array[0]
+end
+
+
+
+
 
 
 
